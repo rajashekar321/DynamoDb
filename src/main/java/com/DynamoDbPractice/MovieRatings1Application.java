8acc7981445502d488c7455afd7ebd813c7059ea
rajashekar321 <43377681+rajashekar321@users.noreply.github.com>
2024-07-17 12:08:49 +0530

    Update MovieRatings1Application.java

--- src/main/java/com/DynamoDbPractice/MovieRatings1Application.java
+++ src/main/java/com/DynamoDbPractice/MovieRatings1Application.java
@@ -39,45 +39,42 @@ public class MovieRatings1Application {
 //		DateTimeFormatter df = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 //		LocalDate dt = LocalDate.parse(s,df);
 //		System.out.println(dt);
-		// AmazonDynamoDB cb = AmazonDynamoDBClient.builder().set
-		// BasicSessionCredentials awsCreds = new
-		// BasicSessionCredentials("AKIAQ64ZE3AYVKQWRE7F",
-		// "B7TEwY2TvhdqfrdvoPnr14h2zIGpRPuPmsdyu6mB");
+		
 
 		AmazonDynamoDB db = AmazonDynamoDBClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(
 						new BasicAWSCredentials("YourAccessKey", "YourSecretAccesskey")))
-				.withRegion(Regions.US_EAST_1).build();
+				.withRegion("Your Region").build();// need to give your aws credentials
 		// AwsBasicCredentials awsbasicCredentials=AwsBasicCredentials.create();
 		// db.getItem(null)
 		// aTable table=db.gett
-		DynamoDB db1 = new DynamoDB(db);
-		Table table = db1.getTable("Student");
+		DynamoDB db1 = new DynamoDB(db);//DynamoDb object have more simplify methods to use crud operations
+		Table table = db1.getTable("Student");//mention your tableName
 		String partitionKey = "Student_Id";
-		String rangeKey = "College_Joined_Date";
-		Map<String, AttributeValue> map = new HashMap<>();
-		map.put("Student_Id", new AttributeValue().withS("15124-CM-017"));
+		String rangeKey = "College_Joined_Date";//sortkey or rangekey
+		Map<String, AttributeValue> map = new HashMap<>(); //need to create a map for fetching details with primarykey and sortkey
+		map.put("Student_Id", new AttributeValue().withS("15190-CM-017"));
 		map.put("College_Joined_Date", new AttributeValue().withS("1721128481"));
 //		Map<String, AttributeValue> insertingElements = new HashMap<>();
-//		insertingElements.put("Student_Id", new AttributeValue().withS("15124-CM-019"));
+//		insertingElements.put("Student_Id", new AttributeValue().withS("15984-CM-019"));
 //		insertingElements.put("College_Joined_Date", new AttributeValue().withS("1721193203"));
-		GetItemSpec spec = new GetItemSpec().withPrimaryKey(partitionKey, "15124-CM-017", rangeKey, "1721128481");
-		Item item = new Item().withPrimaryKey("Student_Id", "15124-CM-019")
+		GetItemSpec spec = new GetItemSpec().withPrimaryKey(partitionKey, "15824-CM-017", rangeKey, "1721128481");//for fetching you can mention directly
+		Item item = new Item().withPrimaryKey("Student_Id", "15184-CM-019")
 				.withString("College_Joined_Date", "1721193203")
 				.withString("Department", "Computer Science and Engineering");
-		PutItemSpec pSpec = new PutItemSpec().withItem(item);
+		PutItemSpec pSpec = new PutItemSpec().withItem(item);//just creare one item that you want to create in table in ddb so you can directly give using with item(item type)
 		// GetItemRequest gir = new
 		// GetItemRequest().withTableName("Student").withKey(map);
 		// PutItemRequest pir=PutItemRequest.builder();
-		ScanSpec sc = new ScanSpec();
+		ScanSpec sc = new ScanSpec();//this specification is used for scan expression
 		try {
-//			ItemCollection<ScanOutcome> so = table.scan(sc);
+//			ItemCollection<ScanOutcome> so = table.scan(sc);//when you want to scan all items in the table 
 //			Iterator<Item> itr = so.iterator();
 //			while (itr.hasNext()) {
-//				System.out.println(itr.next());
+//				System.out.println(itr.next());//printing all the items
 //			}
 			// Item itm = table.getItem(spec);
-			table.putItem(item);
+			table.putItem(item);//inserting /updating single item
 			// GetItemResult giresult = db.getItem(gir);
 			// System.out.println(giresult.toString());
 			// System.out.println(itm);
@@ -87,8 +84,8 @@ public class MovieRatings1Application {
 		}
 		// GetItemRequest gir=GetItemRequest.
 		// GetItemRequest gir = GetItemRequest.builder().withTableName("Student")
-		// .key(Collections.singletonMap("15124-CM-017", "1721128481")).build();
-		// Item itm = table.getItem("15124-CM-017", "1721128481");
+		// .key(Collections.singletonMap("15190-CM-017", "1721128481")).build();
+		// Item itm = table.getItem("15190-CM-017", "1721128481");
 		// System.out.println(itm);
 
 	}
